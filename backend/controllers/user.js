4b41eb6931f3641baa2597c2bcfbc34569285efe
vadym-loik <loikfit@gmail.com>
2023-03-01 15:26:30 +0100

    Update user.js

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -27,68 +27,51 @@ exports.signup = function (req, res, next) {
   });
 };
 
-// exports.signup = (req, res, next) => {
-//   const { name, email, password } = req.body;
+// LOGIN
+// exports.login = async function (req, res, next) {
+//   const { email, password } = req.body;
 
-//   bcrypt.hash(password, 10).then((hash) => {
-//     const user = new User({
-//       name: name,
-//       email: email,
-//       password: hash,
-//       admin: false,
+//   await User.findOne({ where: { email: email, password: password } })
+//     .then(() => {
+//       res.status(200).json({ message: 'User found!' });
+//     })
+//     .catch((error) => {
+//       res.status(400).json({ error });
 //     });
-//     User.create(user).then(() =>
-//       res
-//         .status(201)
-//         .json({ message: 'Account created!' })
-//         .catch((error) => res.status(400).json({ error: error }))
-//     );
-//   });
 // };
 
-// LOGIN
-exports.login = async function (req, res, next) {
+exports.login = (req, res, next) => {
   const { email, password } = req.body;
 
-  await User.findOne({ where: { email: email, password: password } })
-    .then(() => {
-      res.status(200).json({ message: 'User found!' });
-    })
-    .catch((error) => {
-      res.status(400).json({ error });
-    });
+  User.findOne({
+    where: { email: email },
+  }).then((user) => {
+    if (!user) {
+      return res.status(401).json({ error: 'User not found!' });
+    }
+    bcrypt
+      .compare(password, user.password)
+      .then((valid) => {
+        if (!valid) {
+          return res.status(401).json({ error: 'Incorrect password!' });
+        }
+        res.status(200).json({
+          name: user.name,
+          userId: user.id,
+          token: jwt.sign(
+            {
+              userId: user.id,
+            },
+            'RANDOM_TOKEN_SECRET',
+            { expiresIn: '24h' }
+          ),
+        });
+      })
+      .catch((error) => {
+        res.status(500).json({ error: error });
+      });
+  });
 };
-
-// exports.login = (req, res, next) => {
-//   const { email, password } = req.body;
-//   console.log(req.body);
-//   User.findOne({
-//     where: { email: email },
-//   }).then((user) => {
-//     if (!user) {
-//       return res.status(401).json({ error: 'Incorrect e-mail' });
-//     } else {
-//       bcrypt
-//         .compare(password, user.password)
-//         .then((valid) => {
-//           if (!valid) {
-//             return res.status(401).json({ error: 'Incorrect password!' });
-//           }
-
-//           const token = jwt.sign({ userId: user._id }, RANDOM_TOKEN_SECRET, {
-//             expiresIn: '24h',
-//           });
-//           res.status(200).json({
-//             userId: user._id,
-//             token: token,
-//           });
-//         })
-//         .catch((error) => {
-//           res.status(500).json({ error: error });
-//         });
-//     }
-//   });
-// };
 
 // get all users
 exports.getAllUsers = (req, res, next) => {
@@ -99,4 +82,17 @@ exports.getAllUsers = (req, res, next) => {
     .catch((error) => {
       res.status(400).json({ error });
     });
+};
+
+// DELETE USER
+exports.deleteUser = (req, res, next) => {
+  User.destroy({
+    where: {
+      userId: req.params.id,
+    },
+  })
+    .then(() => {
+      res.status(200).json({ message: 'User deleted!' });
+    })
+    .catch((error) => res.status(500).json({ error }));
 };
