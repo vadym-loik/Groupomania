2ebcb8c21390613f62e4e313afa7ec0faac38388
vadym-loik <loikfit@gmail.com>
2023-03-02 13:11:21 +0100

    Create CustomInput.vue

---
+++ frontend/src/shared/CustomInput.vue
@@ -0,0 +1,23 @@
+<template>
+  <input class="custom-input" v-on="$listeners" />
+</template>
+
+<script>
+export default {
+  name: 'CustomInput',
+};
+</script>
+
+<style lang="scss" scoped>
+@import '.././assets/scss/variables.scss';
+
+.custom-input {
+  min-height: 40px;
+  width: 300px;
+  border: 2px solid $main-color;
+  font-size: 18px;
+  outline: none;
+  line-height: inherit;
+  padding: 8px 15px;
+}
+</style>
