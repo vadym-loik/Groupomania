15daf9e305a6734106bf7c87798f79577d02bc53
vadym-loik <loikfit@gmail.com>
2023-03-02 13:11:24 +0100

    Create Footer.vue

---
+++ frontend/src/shared/Footer.vue
@@ -0,0 +1,28 @@
+<template>
+  <footer class="footer">
+    <p class="footer-text">&copy; 2023 connect-e.com</p>
+  </footer>
+</template>
+
+<script>
+export default {
+  name: 'Footer',
+};
+</script>
+
+<style lang="scss" scoped>
+@import '../assets/scss/variables.scss';
+
+.footer {
+  display: flex;
+  flex-direction: column;
+  background: $secondary-color;
+  height: 30px;
+}
+
+.footer-text {
+  text-align: center;
+  margin: auto;
+  color: #fff;
+}
+</style>
