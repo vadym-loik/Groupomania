604789052392d5870903cff21e347b1322bccacc
vadym-loik <loikfit@gmail.com>
2023-03-02 13:11:30 +0100

    Create Logo.vue

---
+++ frontend/src/shared/Logo.vue
@@ -0,0 +1,15 @@
+<template>
+  <img
+    src="../assets/icon-left-font-monochrome-white.svg"
+    alt="logo"
+    class="logo"
+  />
+</template>
+
+<script>
+export default {
+  name: 'Logo',
+};
+</script>
+
+<style lang="scss" scoped></style>
