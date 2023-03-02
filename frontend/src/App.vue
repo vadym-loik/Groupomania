de04f3fab38ab8a635b2fcb09de8b7d0e3f2dc54
vadym-loik <loikfit@gmail.com>
2023-03-02 13:10:10 +0100

    Update App.vue

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,18 +1,51 @@
 <template>
   <div id="app">
-    <Button><span>Click me</span></Button>
+    <Header></Header>
+    <div class="content">
+      <Container>
+        <Button>Enter</Button>
+        <CustomInput></CustomInput>
+      </Container>
+    </div>
+    <Footer></Footer>
   </div>
+  <router-view></router-view>
 </template>
 
 <script>
 import Button from './components/Button.vue';
+import CustomInput from './shared/CustomInput.vue';
+import Footer from './shared/Footer.vue';
+import Container from './shared/Container.vue';
+import Header from './shared/Header.vue';
 
 export default {
   name: 'app',
   components: {
+    Container,
     Button,
+    CustomInput,
+    Footer,
+    Header,
   },
 };
 </script>
 
-<style scoped></style>
+<style lang="scss" scoped>
+* {
+  font-family: 'Roboto Mono', monospace;
+}
+
+#app {
+  display: flex;
+  flex-direction: column;
+  min-height: 100vh;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
+
+.content {
+  flex-grow: 1;
+  padding-top: 120px;
+}
+</style>
