cb07761fe753f74a4e7c185a62f23d16d9c7e01f
vadym-loik <loikfit@gmail.com>
2023-03-02 13:10:54 +0100

    Update Button.vue

--- frontend/src/components/Button.vue
+++ frontend/src/components/Button.vue
@@ -16,4 +16,25 @@ export default {
 };
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.btn {
+  width: 150px;
+  height: 40px;
+
+  cursor: pointer;
+  border-radius: 8px;
+  border: none;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+  font-size: 16px;
+
+  color: #fff;
+  background: #ff662d;
+
+  &:hover {
+    box-shadow: 0 12px 16px 0 rgba(0, 0, 0, 0.24),
+      0 17px 50px 0 rgba(0, 0, 0, 0.19);
+  }
+}
+</style>
