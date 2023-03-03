26f2c801692bcb4bb16aef05635f4d11e7853de7
vadym-loik <loikfit@gmail.com>
2023-03-03 16:26:06 +0100

    Update user.js

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -4,7 +4,7 @@ const validEmail = require('../middleware/email_validator');
 const validPassword = require('../middleware/password_validator');
 const userCtrl = require('../controllers/user');
 
-router.post('/signup', userCtrl.signup);
+router.post('/signup', validEmail, validPassword, userCtrl.signup);
 router.post('/login', userCtrl.login);
 router.get('/', userCtrl.getAllUsers);
 router.delete('/user/:id', userCtrl.deleteUser);
