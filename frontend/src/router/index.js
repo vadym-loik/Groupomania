3ca5c955991cff8dbe7fade42217da633e9c5c5f
vadym-loik <loikfit@gmail.com>
2023-03-03 16:27:27 +0100

    Update index.js

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,10 +1,14 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import Login from '../pages/Login.vue';
 import Signup from '../pages/Signup.vue';
+import HomePage from '../pages/HomePage.vue';
+// import ErrorPage from '../pages/ErrorPage.vue';
 
 const routes = [
-  { path: '/login', name: 'Login', component: Login },
-  { path: '/signup', name: 'Signup', component: Signup },
+  { path: '/login', name: 'login', component: Login },
+  { path: '/signup', name: 'signup', component: Signup },
+  { path: '/', name: 'homepage', component: HomePage },
+  //   { path: '*', name: 'error-page', component: ErrorPage },
 ];
 
 const router = createRouter({
