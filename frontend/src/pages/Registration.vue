12af08ca09bf7aadb54c6633319ec3c737d4db05
vadym-loik <loikfit@gmail.com>
2023-03-09 11:45:38 +0100

    Create Registration.vue

---
+++ frontend/src/pages/Registration.vue
@@ -0,0 +1,26 @@
+<template>
+  <main class="registration">
+    <!-- <SectionWithHeaderSpacer> -->
+    <AuthSection>
+      <RegistrationForm />
+    </AuthSection>
+    <!-- </SectionWithHeaderSpacer> -->
+  </main>
+</template>
+
+<script>
+// import SectionWithHeaderSpacer from '../components/shared/SectionWithHeaderSpacer.vue';
+import AuthSection from '../components/auth/AuthSection.vue';
+import RegistrationForm from '../components/auth/registration/index.vue';
+
+export default {
+  name: 'RegistrationPage',
+  components: {
+    // SectionWithHeaderSpacer,
+    AuthSection,
+    RegistrationForm,
+  },
+};
+</script>
+
+<style lang="scss" scoped></style>
