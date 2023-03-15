f2f9121bec71b155e4bfc653b6767d3577555ee4
vadym-loik <loikfit@gmail.com>
2023-03-15 16:05:37 +0100

    Update main.js

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,8 +1,8 @@
-import { createApp } from 'vue';
 import './assets/scss/index.scss';
-import './axios';
+import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router';
 import { createPinia } from 'pinia';
+import './axios';
 
 createApp(App).use(router).use(createPinia()).mount('#app');
