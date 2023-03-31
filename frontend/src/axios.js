557f0bd8bbad35a8078162ba3bce7d6bba162f93
vadym-loik <loikfit@gmail.com>
2023-03-31 14:32:11 +0200

    Update axios.js

--- frontend/src/axios.js
+++ frontend/src/axios.js
@@ -1,6 +1,19 @@
 import axios from 'axios';
 
-axios.defaults.withCredentials = true;
-axios.defaults.baseURL = 'http://localhost:8000/';
+const Axios = axios.create({
+  baseURL: `${import.meta.env.BASE_URL}`,
+  headers: {
+    'Content-Type': 'application/json',
+  },
+});
 
-export default axios;
+// Set the JWT token in the Authorization header for all requests
+Axios.interceptors.request.use((config) => {
+  const token = localStorage.getItem('token');
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
+});
+
+export default Axios;
