1209432c99e703fbdb9df1b39a4ca38d177b8ea1
vadym-loik <loikfit@gmail.com>
2023-03-31 14:32:21 +0200

    Update authStore.js

--- frontend/src/stores/authStore.js
+++ frontend/src/stores/authStore.js
@@ -1,34 +1,68 @@
 import { defineStore } from 'pinia';
-import axios from '../axios';
-import { router } from '../router/index';
-
-const baseUrl = `${import.meta.env.BASE_URL}/login`;
+import Axios from '../axios';
+import router from '../router';
 
 export const useAuthStore = defineStore({
   id: 'auth',
   state: () => ({
-    user: JSON.parse(localStorage.getItem('user')),
-    returnUrl: null,
+    user: null,
+    token: localStorage.getItem('token'),
   }),
-  getters: {},
+  getters: {
+    isAuthenticated: (state) => !!state.token,
+  },
   actions: {
     async login(email, password) {
       try {
-        const user = await axios.post(`${baseUrl}/authenticate`, {
+        const response = await Axios.post('/login', {
           email,
           password,
         });
+        const { user, token } = response.data;
         this.user = user;
-        localStorage.setItem('user', JSON.stringify(user.token));
+        this.token = token;
+        localStorage.setItem('token', token);
         router.push(this.returnUrl || '/');
+        console.log(user);
+        return user;
       } catch (error) {
         console.log(error);
       }
     },
-    logout() {
+    async logout() {
       this.user = null;
-      localStorage.removeItem('user');
+      this.token = null;
+      localStorage.removeItem('token');
       router.push('/account/login');
     },
   },
 });
+
+// export const useAuthStore = defineStore({
+//   id: 'auth',
+//   state: () => ({
+//     user: JSON.parse(localStorage.getItem('user')),
+//     returnUrl: null,
+//   }),
+//   getters: {},
+//   actions: {
+//     async login(email, password) {
+//       try {
+//         const user = await axios.post(`${baseUrl}/authenticate`, {
+//           email,
+//           password,
+//         });
+//         this.user = user;
+//         localStorage.setItem('user', JSON.stringify(user.token));
+//         router.push(this.returnUrl || '/');
+//       } catch (error) {
+//         console.log(error);
+//       }
+//     },
+//     logout() {
+//       this.user = null;
+//       localStorage.removeItem('user');
+//       router.push('/account/login');
+//     },
+//   },
+// });
