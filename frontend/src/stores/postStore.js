648cadfd9626a04015e13816dbcce1fa1fa2e9bb
vadym-loik <loikfit@gmail.com>
2023-04-29 01:05:48 +0200

    Update postStore.js

--- frontend/src/stores/postStore.js
+++ frontend/src/stores/postStore.js
@@ -7,57 +7,47 @@ export const usePostStore = defineStore({
     posts: [],
     post: null,
     loading: false,
-    error: null,
   }),
   getters: {},
   actions: {
-    async fetchPosts() {
-      try {
-        this.posts = await Axios.get(`/api/posts/`).then(
-          (response) => response.data
-        );
-      } catch (error) {
-        this.error = error;
-      } finally {
-        this.loading = false;
-      }
+    setPosts(postList) {
+      this.posts = postList;
     },
-    async fetchPostById(id) {
-      try {
-        this.post = await Axios.get(`/api/posts/${id}`).then(
-          (response) => response.data
-        );
-      } catch (error) {
-        this.error = error;
-      } finally {
-        this.loading = false;
-      }
+    getOnePost(onePost) {
+      this.post = onePost;
     },
-    async createPost(post) {
-      try {
-        const response = await Axios.post(`/api/posts`, post);
-        this.posts.push(response.data);
-      } catch (error) {
-        this.error = error;
-      }
+    addNewPost(newPost) {
+      this.posts.push(newPost);
     },
-    async updatePost(post) {
-      try {
-        const response = await Axios.put(`/api/posts/${post.id}`, post);
-        this.posts = this.posts.map((p) =>
-          p.id === response.data.id ? response.data : p
-        );
-      } catch (error) {
-        this.error = error;
-      }
-    },
-    async deletePost(postId) {
-      try {
-        await Axios.delete(`/api/posts/${postId}`);
-        this.posts = this.posts.filter((p) => p.id !== postId);
-      } catch (error) {
-        this.error = error;
-      }
-    },
+    // async function fetchPostById(id) {
+    //   try {
+    //     this.post = await Axios.get(`/api/posts/${id}`).then(
+    //       (response) => response.data
+    //     );
+    //   } catch (error) {
+    //     this.error = error;
+    //   } finally {
+    //     this.loading = false;
+    //   }
+    // },
+
+    //   async updatePost(post) {
+    //     try {
+    //       const response = await Axios.put(`/api/posts/${post.id}`, post);
+    //       this.posts = this.posts.map((p) =>
+    //         p.id === response.data.id ? response.data : p
+    //       );
+    //     } catch (error) {
+    //       this.error = error;
+    //     }
+    //   },
+    //   async deletePost(postId) {
+    //     try {
+    //       await Axios.delete(`/api/posts/${postId}`);
+    //       this.posts = this.posts.filter((p) => p.id !== postId);
+    //     } catch (error) {
+    //       this.error = error;
+    //     }
+    //   },
   },
 });
