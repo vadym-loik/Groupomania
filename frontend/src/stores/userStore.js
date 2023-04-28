4aa4e9598bf1397acec1445f60beeb77bb98ff88
vadym-loik <loikfit@gmail.com>
2023-04-29 01:05:53 +0200

    Update userStore.js

--- frontend/src/stores/userStore.js
+++ frontend/src/stores/userStore.js
@@ -4,13 +4,24 @@ import Axios from '../api';
 export const useUserStore = defineStore({
   id: 'user',
   state: () => ({
-    user: [],
+    user: {
+      // id: 13,
+      // name: 'Lili',
+      // email: 'lili@fr.fr',
+      // imageUrl: null,
+      // password: '$2b$10$vzZ81vNZMXMo.8O2HKPgxucJSR3vAw2oiTWLIBFPZ50eSjCtPIQ06',
+      // isAdmin: false,
+      // createdAt: '2023-04-25T13:34:49.000Z',
+      // updatedAt: '2023-04-25T13:34:49.000Z',
+    },
   }),
   actions: {
-    async getOneUser(id) {
-      this.user = await Axios.get(`/api/auth/profile/${id}`).then(
-        (response) => response.data
-      );
+    setUser(userById) {
+      this.user = userById;
+      console.log(this.user);
+    },
+    modifyUserName(newName) {
+      this.user.name = newName;
     },
   },
 });
