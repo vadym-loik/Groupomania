6dfc50019eb45cd2e3d65498d152061beb369abc
vadym-loik <loikfit@gmail.com>
2023-04-29 01:03:53 +0200

    Update post.js

--- backend/routes/post.js
+++ backend/routes/post.js
@@ -4,10 +4,10 @@ const postCtrl = require('../controllers/post');
 const multer = require('../middleware/multer_config');
 const auth = require('../middleware/auth');
 
-router.post('/', multer, postCtrl.createPost);
-router.get('/:id', postCtrl.getOnePost);
-router.get('/', postCtrl.getAllPosts);
-router.put('/:id', multer, postCtrl.modifyPost);
-router.delete('/:id', postCtrl.deletePost);
+router.post('/', auth, multer, postCtrl.createPost);
+router.get('/:id', auth, postCtrl.getOnePost);
+router.get('/', auth, postCtrl.getAllPosts);
+router.put('/:id', auth, multer, postCtrl.modifyPost);
+router.delete('/:id', auth, postCtrl.deletePost);
 
 module.exports = router;
