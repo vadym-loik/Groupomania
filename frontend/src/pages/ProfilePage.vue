777999785ee15150a095583b951be4d8f3661427
vadym-loik <loikfit@gmail.com>
2023-04-29 01:05:05 +0200

    Update ProfilePage.vue

--- frontend/src/pages/ProfilePage.vue
+++ frontend/src/pages/ProfilePage.vue
@@ -11,54 +11,6 @@
 import Profile from '../components/profile/Profile.vue';
 import SectionWithHeaderSpacer from '../components/SectionWithHeaderSpacer.vue';
 import Container from '../components/Container.vue';
-import { useUserStore } from '../stores/userStore';
-
-import { ref, onMounted } from 'vue';
-
-const user = ref([]);
-const userStore = useUserStore();
-
-function getUserById() {
-  user.value = userStore.getOneUser();
-}
-
-onMounted(getUserById);
-
-// getUserById();
-
-// function getUsers() {
-//   axios
-//     .get('http://localhost:8000/api/auth/profile/:id')
-//     .then((res) => {
-//       users.value = res.data;
-//       console.log(users.value);
-//     })
-//     .catch((err) => {
-//       console.log(err);
-//     });
-// }
-
-// onMounted(() => {
-//   getUsers();
-// });
-// import { getUserData } from '../api.js';
-
-// export default {
-//   components: {
-//     Profile,
-//     SectionWithHeaderSpacer,
-//     Container,
-//   },
-//   data() {
-//     return {
-//       currentUser: {},
-//     };
-//   },
-//   mounted() {
-//     // this.currentUser = getUserData();
-//   },
-//   methods: {},
-// };
 </script>
 
 <style lang="scss" scoped>
