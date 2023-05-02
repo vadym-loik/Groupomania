411c94e44e5b55287975fd700b4fae2f6a471bad
vadym-loik <loikfit@gmail.com>
2023-05-02 17:11:04 +0200

    Update PostsList.vue

--- frontend/src/components/posts/PostsList.vue
+++ frontend/src/components/posts/PostsList.vue
@@ -10,43 +10,22 @@
 import Container from '../Container.vue';
 import PostCard from './PostCard.vue';
 import { ref, onMounted } from 'vue';
-import { usePostStore } from '@/stores/postStore';
+
 import Axios from '@/api';
 
-const postStore = usePostStore();
-
 const posts = ref([]);
-const post = ref(null);
-const loading = ref(false);
 
 async function getAllPosts() {
   try {
-    await Axios.get(`/api/posts/`).then((response) => {
-      postStore.setPosts(response.data);
-      posts.value = postStore.posts;
+    await Axios.get(`/api/posts/`).then((res) => {
+      posts.value = res.data;
     });
   } catch (error) {
     console.log(error);
-  } finally {
-    loading.value = postStore.loading;
   }
 }
 
-// async function getOnePost(id) {
-//   try {
-//     await Axios.get(`/api/posts/${id}`).then((response) => {
-//       postStore.getOnePost(response.data);
-//       post.value = postStore.post;
-//     });
-//   } catch (error) {
-//     console.log(error);
-//   } finally {
-//     loading.value = postStore.loading;
-//   }
-// }
-
 onMounted(getAllPosts);
-// onMounted(getOnePost);
 </script>
 
 <style lang="scss" scoped>
